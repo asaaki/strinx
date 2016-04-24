185e89c2eaef4d449560de077921768989db4b3e
Christoph Grabo <asaaki@mannaz.cc>
2016-04-24 17:59:00 +0200

    Cleanup code

--- lib/strinx.ex
+++ lib/strinx.ex
@@ -1,38 +1,56 @@
 defmodule Strinx do
+  @moduledoc """
+  Some string transformation functions for Elixir
 
-  # underscores to camelcases
+  Heavily inspired by ActiveSupport's String extensions (Ruby).
+  """
 
-  def camelcase(input), do: camelize(input, "", true)
+  alias String.Unicode
 
-  def camelcase(input, first_letter_flag), do: camelize(input, "", first_letter_flag)
+  @doc "`foo_bar` to `FooBar`"
+  def camelcase(input),
+    do: camelize(input, "", true)
 
-  def camelize(""),    do: ""
-  def camelize(input), do: camelize(input, "", true)
+  @doc """
+  Like Strinx.camelcase/1, but you can specify if the first letter should be
+  lowercase by setting `first_letter_flag` to `:lower`.
 
-  def camelize("", _first_letter_flag), do: ""
-  def camelize(input, first_letter_flag) do
-    to_upper = first_letter_flag != :lower
-    camelize(input, "", to_upper)
-  end
+  ## Example
+
+      Strinx.camelcase("foo_bar", :lower)
+      #=> "fooBar"
+  """
+  def camelcase(input, first_letter_flag),
+    do: camelize(input, first_letter_flag)
+
+  @doc false
+  def camelize(input),
+    do: camelize(input, "", true)
+
+  @doc false
+  def camelize("", _),
+    do: ""
+  def camelize(input, first_letter_flag),
+    do: camelize(input, "", first_letter_flag != :lower)
 
   defp camelize("", output, _to_upper), do: output
-  defp camelize(<< "_", rest :: binary >>, output, _to_upper) do
-    camelize(rest, output, true)
-  end
-  defp camelize(<< char :: utf8, rest :: binary >>, output, to_upper) do
-    char = if to_upper, do: String.upcase(<< char :: utf8 >>), else: << char :: utf8 >>
-    camelize(rest, output <> char, false)
-  end
+  defp camelize(<< "_", rest :: binary >>, output, _to_upper),
+    do: camelize(rest, output, true)
+  defp camelize(<< char :: utf8, rest :: binary >>, output, to_upper),
+    do: camelize(rest, output <> maybe_upcase(to_upper, char), false)
 
-  # underscores to dashes
+  defp maybe_upcase(true, char),
+    do: String.upcase(<< char :: utf8 >>)
+  defp maybe_upcase(false, char),
+    do: << char :: utf8 >>
 
-  def dasherize(input), do: String.replace(input, "_", "-")
+  @doc "`foo_bar` to `foo-bar`"
+  def dasherize(input),
+    do: String.replace(input, "_", "-")
 
-  # camelcases to underscores
-
+  @doc "`FooBar` to `foo_bar`"
   # FIXME: "FooBar FuuBaz" => "foo_bar _fuu_baz"
   # inner leading underscores (` _fuu_baz` => ` fuu_baz`) need to be eliminated, too.
-
   def underscore(input) do
     input
     |> String.graphemes
@@ -41,17 +59,19 @@ defmodule Strinx do
     |> strip_leading_underscore
   end
 
-  defp underscore_char(char) do
-    case noncapital?(char) do
-      true  -> char
-      false -> "_" <> String.Unicode.downcase(char)
-    end
-  end
+  defp underscore_char(char),
+    do: maybe_underscore(noncapital?(char), char)
 
-  defp noncapital?(char) do
-    String.Unicode.downcase(char) == char
-  end
+  defp maybe_underscore(true, char),
+    do: char
+  defp maybe_underscore(false, char),
+    do: "_" <> Unicode.downcase(char)
 
-  defp strip_leading_underscore(<< "_", output :: binary >>), do: output
-  defp strip_leading_underscore(output), do: output
+  defp noncapital?(char),
+    do: Unicode.downcase(char) == char
+
+  defp strip_leading_underscore(<< "_", output :: binary >>),
+    do: output
+  defp strip_leading_underscore(output),
+    do: output
 end
