f3dd29205d23f5f8264af0e98305f5fdcb1e0d83
Christoph Grabo <asaaki@mannaz.cc>
2016-04-24 17:59:59 +0200

    Bump version to v0.2.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Strinx.Mixfile do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
   @project_url "https://github.com/asaaki/strinx.ex"
   @docs_url "http://hexdocs.pm/strinx"
 
